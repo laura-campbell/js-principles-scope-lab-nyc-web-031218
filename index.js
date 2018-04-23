b884afb50e757242dd639699d715ff76bf011b8b
laura-campbell <campbell.laura.elizabeth@gmail.com>
2018-04-23 15:12:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(x) {
+  bestCustomer = x;
+}
+const leastFavoriteCustomer = 'laura';
+
+function changeLeastFavoriteCustomer(x) {
+  leastFavoriteCustomer = x;
+}
